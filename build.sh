4edc896ada77b4b88fade4a45bd9feddf369735e
Auston Stewart <auston@shedbuilt.net>
2018-05-30 16:33:26 -1000

    Updating to v3.3.15 for blank

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
+# Configure
 ./configure --prefix=/usr                            \
             --exec-prefix=                           \
             --libdir=/usr/lib                        \
-            --docdir=/usr/share/doc/procps-ng-3.3.12 \
+            --docdir="$SHED_PKG_LOCAL_DOCDIR"        \
             --disable-static                         \
             --disable-kill                           \
-            --with-systemd
-make -j $SHED_NUM_JOBS
-make DESTDIR=${SHED_FAKE_ROOT} install
-mkdir -v ${SHED_FAKE_ROOT}/lib
-mv -v ${SHED_FAKE_ROOT}/usr/lib/libprocps.so.* ${SHED_FAKE_ROOT}/lib
-ln -sfv ../../lib/$(readlink ${SHED_FAKE_ROOT}/usr/lib/libprocps.so) ${SHED_FAKE_ROOT}/usr/lib/libprocps.so
+            --with-systemd &&
+# Build and Configure
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+# Rearrage
+mkdir -v "${SHED_FAKE_ROOT}/lib" &&
+mv -v "${SHED_FAKE_ROOT}/usr/lib"/libprocps.so.* "${SHED_FAKE_ROOT}/lib" &&
+ln -sfv ../../lib/$(readlink "${SHED_FAKE_ROOT}/usr/lib/libprocps.so") "${SHED_FAKE_ROOT}/usr/lib/libprocps.so" || exit 1
+# Optionally Purge Documentation
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    rm -rf "${SHED_FAKE_ROOT}/usr/share/doc"
+fi
